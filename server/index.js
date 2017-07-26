fb4cf236a95bf328a8d55ca959be6c0d997fd0f0
Stephanie Fu <StephaniefuU@gmail.com>
2017-07-25 19:29:55 -0700

    successfully connected to port

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
 // require necessary imports
+const express = require('express');
+const parser = require('body-parser');
+//instantiate express
+const app = express();
 
+//middleware
+//chunking all your data so it can be put into a json file and sent forward
+app.use(parser.urlencoded({extended: true}));
 // have express instance listen in on a PORT
+
+const PORT = 3000;
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log(`Could not connect to server at port ${PORT}`);
+  } else {
+    console.log(`Successfully connected to PORT ${PORT}`);
+  }
+})
